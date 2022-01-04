d6c072a40fdb2cc250bb83765aa7b41eb194f58d
Cienna97 <cmortenson97@gmail.com>
2022-01-03 17:59:56 -0800

    defining collumns in category.js

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,32 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+     
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8]
+      }
+      }
   },
   {
     sequelize,
