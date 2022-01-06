fe7d1e208bf08a98e01a320d74e4e917f64609cb
Cienna97 <cmortenson97@gmail.com>
2022-01-06 03:26:41 -0700

    finsihing writing index.js imports

--- models/index.js
+++ models/index.js
@@ -11,17 +11,28 @@ const ProductTag = require('./ProductTag');
 //   foreignKey: 'user_id'
 // });
 
+// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
-})
+});
 
-// Products belongsTo Category
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
