3f8b74c0f7489f222e3aef76fdc336f57784de61
Cienna97 <cmortenson97@gmail.com>
2022-01-06 05:04:42 -0700

    working on GET, POST, AND PUT routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 //const res = require('express/lib/response');
-//const { UPSERT } = require('sequelize/dist/lib/query-types');
+const { UPSERT } = require('sequelize/dist/lib/query-types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -31,24 +31,61 @@ router.get('/', (req, res) => {
 });
  
 
-  });
- 
-
-
 router.post('/', (req, res) => {
-
- 
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(categories => res.json(categories))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+ 
 
 router.put('/:id', (req, res) => {
-
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categories => {
+      if (!categories) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(categories);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
     
 
     });
 
 
 router.delete('/:id', (req, res) => {
-
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categories => {
+    if (!categories) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(categories);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 
 });
 
