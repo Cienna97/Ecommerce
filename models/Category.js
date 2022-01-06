cb9209bc8443a3c716d7d6df60228253df470fdf
Cienna97 <cmortenson97@gmail.com>
2022-01-06 03:08:01 -0700

    adding to content to models

--- models/Category.js
+++ models/Category.js
@@ -12,30 +12,12 @@ Category.init(
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
-    email: {
+    category_name: {
       type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-      validate: {
-        len: [8]
-      }
-      }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+      allowNull: false
+    }
   }
 );
 
